8c2e0342716674541f4befa1fed0e4024b48092d
MattDekinder <dekinder@ualberta.ca>
2017-01-10 09:03:04 -0700

    changed script to get itself

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import requests
-r = requests.get("http://google.com/")
+r = requests.get("https://raw.githubusercontent.com/MattDekinder/cmput404lab1/master/script.py")
 
 print r.status_code
+print r.text
